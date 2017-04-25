74b939a715d9201cd59a71f02ffeb97f7a96409d
balexand <balexand@gmail.com>
2017-04-25 10:13:26 +0200

    parse IGC date

--- lib/igc.ex
+++ lib/igc.ex
@@ -3,16 +3,40 @@ defmodule Igc do
   Documentation for Igc.
   """
 
-  @doc """
-  Hello world.
+  @doc ~S"""
+  Parses an IGC file.
 
   ## Examples
 
-      iex> Igc.hello
-      :world
-
+      iex> Igc.parse("HFDTE280709\nB1101355206343N00006198WA0058700558")
+      {:ok, %Igc.Track{date: ~D[2009-07-28]}}
   """
-  def hello do
-    :world
+  def parse(str) when is_binary(str) do
+    {:ok, io} = StringIO.open(str)
+    parse(io)
   end
+
+  def parse(io) when is_pid(io) do
+    parse(%Igc.Track{}, io)
+  end
+
+  defp parse(track, io) do
+    case IO.read(io, :line) do
+      :eof -> {:ok, track}
+      line when is_binary(line) ->
+        case handle_line(track, String.trim(line)) do
+          {:ok, track} -> parse(track, io)
+          {:error, detail} -> {:error, detail}
+        end
+    end
+  end
+
+  defp handle_line(track, <<"HFDTE", ddmmyy::binary>>) do
+    case Timex.parse(ddmmyy, "{0D}{0M}{YY}") do
+      {:ok, datetime} -> {:ok, %{track | date: Timex.to_date(datetime)}}
+      {:error, _} -> {:error, "invalid date: HFDTE#{ddmmyy}"}
+    end
+  end
+
+  defp handle_line(track, _line), do: {:ok, track} # TODO ensure test coverage
 end
--- test/igc_test.exs
+++ test/igc_test.exs
@@ -1,8 +1,14 @@
 defmodule IgcTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Igc
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "parse/1" do
+    test "with invalid HFDTE" do
+      assert Igc.parse("HFDTE320709\nB1101355206343N00006198WA0058700558") ==
+        {:error, "invalid date: HFDTE320709"}
+
+      assert Igc.parse("HFDTEXX0709\nB1101355206343N00006198WA0058700558") ==
+        {:error, "invalid date: HFDTEXX0709"}
+    end
   end
 end
