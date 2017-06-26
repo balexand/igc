63b252be8f479c53e7078303798aa5c764a24e3f
balexand <balexand@gmail.com>
2017-06-26 13:31:51 +0200

    organize test

--- test/igc_test.exs
+++ test/igc_test.exs
@@ -4,16 +4,19 @@ defmodule IgcTest do
   use ExUnit.Case, async: true
   doctest Igc
 
+  @valid_date "HFDTE280709\n"
+
+  @valid_points """
+  B1101355206343N00006198WA0058700558
+  B1101455306259N00006295WA0059300556
+  B1101555406300N00006061WA0060300576
+  """
+
+  @valid_igc @valid_date <> @valid_points
+
   describe "parse/1" do
     test "with valid data" do
-      igc = """
-      HFDTE280709
-      B1101355206343N00006198WA0058700558
-      B1101455306259N00006295WA0059300556
-      B1101555406300N00006061WA0060300576
-      """
-
-      {:ok, track} = Igc.parse(igc)
+      {:ok, track} = Igc.parse(@valid_igc)
 
       assert length(track.points) == 3
       assert Enum.map(track.points, &(Map.take(&1, [:latitude, :datetime]))) ==
@@ -48,27 +51,27 @@ defmodule IgcTest do
     end
 
     test "with unknown lines" do
-      assert {:ok, %Track{}} = Igc.parse("HFDTE280709\nHFWTF\nB1101355206343N00006198WA0058700558")
+      assert {:ok, %Track{}} = Igc.parse("HFWTF\n" <> @valid_igc)
     end
 
     test "with invalid HFDTE" do
-      assert Igc.parse("HFDTE320709\nB1101355206343N00006198WA0058700558") ==
+      assert Igc.parse("HFDTE320709\n#{@valid_points}") ==
         {:error, "invalid date: \"HFDTE320709\""}
 
-      assert Igc.parse("HFDTEXX0709\nB1101355206343N00006198WA0058700558") ==
+      assert Igc.parse("HFDTEXX0709\n#{@valid_points}") ==
         {:error, "invalid date: \"HFDTEXX0709\""}
 
-      assert Igc.parse("HFDTE3107090\nB1101355206343N00006198WA0058700558") ==
+      assert Igc.parse("HFDTE3107090\n#{@valid_points}") ==
         {:error, "invalid date: \"HFDTE3107090\""}
     end
 
     test "with invalid trackpoint" do
-      assert Igc.parse("HFDTE280709\nB1101355206343X00006198WA0058700558") ==
+      assert Igc.parse(@valid_igc <> "B1101355206343X00006198WA0058700558") ==
         {:error, "invalid track point: \"B1101355206343X00006198WA0058700558\""}
     end
 
     test "without date" do
-      assert Igc.parse("B1101355206343N00006198WA0058700558") ==
+      assert Igc.parse(@valid_points) ==
         {:error, "file must include date"}
     end
   end
