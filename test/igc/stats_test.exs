300f2928b092f3e8a83ac6904243a64f62af6c9e
balexand <balexand@gmail.com>
2017-06-26 19:19:13 +0200

    change test to read from IO instead of string

--- test/igc/stats_test.exs
+++ test/igc/stats_test.exs
@@ -4,11 +4,9 @@ defmodule Igc.StatsTest do
   alias Igc.Stats
 
   test "calculate/1" do
-    %Stats{} = stats =
-      "test/fixtures/2017-06-17-XCT-XXX-01.igc"
-      |> File.read!
-      |> Igc.parse!
-      |> Stats.calculate
+    %Stats{} = stats = File.open!("test/fixtures/2017-06-17-XCT-XXX-01.igc", [:read_ahead, :utf8], fn f ->
+      f |> Igc.parse! |> Stats.calculate
+    end)
 
     assert stats == %Stats{
       distance: 99_211,
